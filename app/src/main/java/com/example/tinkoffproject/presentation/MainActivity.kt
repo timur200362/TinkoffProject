a0c61c53eda5b3c767c8ebcc78e88ac438c9d9e2
Timur <timurka.askarov@inbox.ru>
2023-04-03 18:32:57 +0300

    test

--- app/src/main/java/com/example/tinkoffproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tinkoffproject/presentation/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.tinkoffproject.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.tinkoffproject.R
+import com.example.tinkoffproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var controller:NavController
+    private var binding:ActivityMainBinding?=null
+
+    override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater).also{
+            setContentView(it.root)
+        }
+        controller=(supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.mainFragment),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+        findViewById<Toolbar>(androidx.appcompat.R.id.action_bar)
+            .setupWithNavController(controller, appBarConfiguration)
+
+        binding?.run {
+            bnvMain.setupWithNavController(controller)
+        }
+    }
+    override fun onBackPressed() {
+        binding?.run {
+            if(bnvMain.selectedItemId!=R.id.mainFragment) {
+                bnvMain.selectedItemId=R.id.mainFragment
+            } else{
+                super.onBackPressed()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tinkoffproject/presentation/MainPage.kt
+++
@@ -1,4 +0,0 @@
-package com.example.tinkoffproject.presentation
-
-class MainPage {
-}
